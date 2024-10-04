18794ac8693d0da41a1ed97766b21ca145ae12d2
Xusanboy <xusanboy@gmail.com>
2024-10-04 12:30:52 +0500

    All are Finished 1.4

--- src/main/java/uz/pdp/awstest/contoller/TestController.java
+++ src/main/java/uz/pdp/awstest/contoller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class TestController {
 
     @PostMapping("/test")
